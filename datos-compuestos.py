7483288d742b83944fb9f75eff45b93ba9e8fec2
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-04-24 15:17:12 +0200

    First commit

---
+++ datos-compuestos.py
@@ -0,0 +1,8 @@
+lista = ['Soy Diego', 'Quiero trabajar ' ' en Python', ' Soy programador', True, 1.75, ' cm'];
+
+
+print(lista[0]) # Imprime el primer elemento de la lista
+print(lista[1]) # Imprime el segundo elemento de la lista
+
+
+print(type(1234)) # Imprime el tipo de dato de la cadena
---
+++ variables.py
@@ -0,0 +1,26 @@
+nombre = 'Diego'
+bienvenida = f'Hola {nombre} espero que estes bien! '
+del nombre
+#"Diego" mit in ist es drin und mit not in es falso
+print("Diego" not in bienvenida)
+
+myvariable = 'Diego dice que '
+new_variable = 'Python es genial'
+end_var = '!'
+print(myvariable + new_variable + end_var)
+print(type(print(myvariable + new_variable + end_var))) # Tipo 'NoneType'
+
+
+# Funciones del sistema
+print(len(bienvenida)) # Imprime la longitud de la cadena
+print(bienvenida.upper()) # Imprime la cadena en mayúsculas 
+print(bienvenida.lower()) # Imprime la cadena en minúsculas
+print(bienvenida.strip()) # Elimina los espacios en blanco al principio y al final de la cadena
+ # print(bienvenida.replace('Diego', '')) # Reemplaza 'Diego' por 'Diego y su hermano'
+print(bienvenida.split()) # Divide la cadena en una lista de palabras
+
+# inputs
+first_name = input('¿Cuál es tu nombre? ')
+age = input('¿Cuántos años tienes? ')
+locaation = input('¿Dónde vives? ')
+print(f'Hola {first_name}, tienes {age} años y vives en {locaation}.') 
