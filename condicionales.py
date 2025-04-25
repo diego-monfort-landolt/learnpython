5d427a94a3ebfa6889f9651e2d7f7382f53ece4e
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-04-25 14:21:45 +0200

    weitere übungen elseif

--- condicionales.py
+++ condicionales.py
@@ -35,7 +35,7 @@ else: print("Necesitas mejorar")
 
 
 # Verificar si un número es par o impar
-numero = int(input("Ingresa un número: "))
+numero = int(input("Averiguamos si tu numero es par o inpar, Ingresa un número: "))
 
 # Usamos el operador módulo (%) para comprobar si el residuo es 0
 if numero % 2 == 0:
@@ -70,3 +70,29 @@ elif horas_de_trabajo == 8:
 else:
     print("Has trabajado menos de 8 horas")
   
+# Tempratura
+  
+tempratura = float(input("Ingresa la temperatura en grados Celsius: "))
+
+if tempratura == 0:
+    print("Hace Frío...")
+elif tempratura > 0 and tempratura <= 20:
+    print("Se está perfecto, no hace calor ni frío")
+elif tempratura > 20 and tempratura <= 30:
+    print("Hace calor")
+else:
+    print("Hace calor extremo")
+    
+# Eingabe: Alter des Benutzers
+alter = int(input("Ingresa tu edad: "))
+
+# Alterskategorien überprüfen
+if alter >= 0 and alter <= 12:
+   print('eres un niño')
+elif alter >= 13 and alter <= 19:
+    print('eres un adolescente')
+elif alter >= 20 and alter <= 65:
+    print('eres un adulto')
+else:
+  print('eres un anciano')
+
