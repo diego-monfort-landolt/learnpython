04ee0a3e676b24b0454f1810e212323c156e6d00
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-04-26 00:27:49 +0200

    alertas en py

--- alert.py
+++ alert.py
@@ -5,4 +5,32 @@ importar from tkinter import messagebox
 messagebox.showinfo("Hinweis", "Achtung das ist ein Test")
 da un mensaje alerta en windows abre ventana de alerta
 """
+# muestra un mensaje de información
 messagebox.showinfo("Hinweis", "Achtung das ist ein Test")
+# muestra un mensaje de información
+messagebox.showinfo("Info", "Das ist eine Info-Meldung!")
+# muestra un mensaje de advertencia
+messagebox.showwarning("Warnung", "Das ist eine Warnung!")
+# muestra un fallo
+messagebox.showerror("Fehler", "Das ist ein Fehler!")
+
+# confirmar si el usuario quiere salir
+antwort = messagebox.askquestion("Frage", "Möchten Sie fortfahren?")
+if antwort == "yes":
+    print("Benutzer möchte fortfahren.")
+    
+# quieres guardar?
+antwort = messagebox.askokcancel("Bestätigung", "Möchten Sie speichern?")
+if antwort:
+    print("Speichern bestätigt.")
+
+# estas seguro
+antwort = messagebox.askyesno("Frage", "Sind Sie sicher?")
+if antwort:
+    print("Benutzer hat 'Ja' gewählt.")    
+    
+# Probar de nuevo o no
+antwort = messagebox.askretrycancel("Fehler", "Möchten Sie es erneut versuchen?")
+if antwort:
+    print("Erneut versuchen ausgewählt.")    
+    
