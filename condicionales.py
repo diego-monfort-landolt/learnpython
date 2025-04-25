d75ad0ecdf6f6c0eca62ea81d4c581ca0e3e1c33
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-04-25 14:03:06 +0200

    aprendiendo ifelifelse

--- condicionales.py
+++ condicionales.py
@@ -23,6 +23,7 @@ else:
    instrucciones
   """
 calificacion = 85
+
 if calificacion >= 90:
     print("Excelente")
 elif calificacion >= 80:
@@ -31,6 +32,8 @@ elif calificacion >= 70:
     print("Bien")
 else: print("Necesitas mejorar")
 # Ejemplo de uso de if-elif-else
+
+
 # Verificar si un número es par o impar
 numero = int(input("Ingresa un número: "))
 
@@ -39,3 +42,31 @@ if numero % 2 == 0:
     print("El número es par.")
 else:
     print("El número es impar.")
+    
+    
+    
+estado_batteria = int(input("Ingresa tu porcentaje de Bateria: "))
+
+if estado_batteria >= 90:
+    print("Batería llena")
+elif estado_batteria >= 80: 
+    print("Batería alta")
+elif estado_batteria >= 70:
+    print("Batería ok")
+elif estado_batteria >= 50:   
+    print("Batería Media")
+elif estado_batteria >= 30:
+    print("Batería baja")
+else:
+    print("Batería crítica")
+    
+  # test de if-elif-else
+    
+horas_de_trabajo = int(input("Ingresa tus Horas Trabajadas: "))
+if horas_de_trabajo > 8:
+    print("Has trabajado más de 8 horas")
+elif horas_de_trabajo == 8:
+    print("Has trabajado exactamente 8 horas")
+else:
+    print("Has trabajado menos de 8 horas")
+  
