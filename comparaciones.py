889af931e1ee4feeb430a13b39d2809a97ea50d4
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-04-25 13:11:14 +0200

    add comparaciones en py

---
+++ comparaciones.py
@@ -0,0 +1,10 @@
+a = 10
+b = 3
+
+
+igual = a == b   # False
+diferente = a != b   # True
+mayor_que = a > b   # True
+menor_que = a < b   # False
+mayor_o_igual = a >= b   # True
+menor_o_igual = a <= b   # False
