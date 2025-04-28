3346e7f358e89c8a73e03ec2188f4bbe661a8975
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-04-28 09:11:13 +0200

    Hinzufügen weitere funktionen verschiedene methoten

--- funciones.py
+++ funciones.py
@@ -15,3 +15,23 @@ def suma(a, b):
 
 resultado = suma(3, 4)
 print(resultado)  # Imprime 7
+
+cuadrado = lambda x: x ** 2
+print(cuadrado(5))  # Imprime 25
+
+def funcion():
+    variable_local = 10
+    print(variable_local)  # Accesible dentro de la función
+
+
+variable_global = 20
+
+
+def funcion2():
+    print(variable_global)  # Accesible desde cualquier lugar
+
+
+funcion()  # Imprime 10
+funcion2()  # Imprime 20
+print(variable_global)  # Imprime 20
+print(variable_local)  # Genera un error, la variable no está definida en este alcance.
