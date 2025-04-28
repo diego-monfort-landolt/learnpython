c7662e414766e76bc5db30c171da80f1a2daf6b4
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-04-28 10:12:16 +0200

    try -exept

---
+++ excepciones.py
@@ -0,0 +1,4 @@
+"""
+Try
+El bloque try contiene el código que puede generar una excepción. Si ocurre una excepción dentro del bloque try, el flujo de ejecución se transfiere al bloque except correspondiente.
+"""
