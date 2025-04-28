5b34bb1526fdb348a0059bf47e0173e29d5de534
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-04-28 10:15:00 +0200

    try - except und finally

--- excepciones.py
+++ excepciones.py
@@ -2,3 +2,37 @@
 Try
 El bloque try contiene el código que puede generar una excepción. Si ocurre una excepción dentro del bloque try, el flujo de ejecución se transfiere al bloque except correspondiente.
 """
+try:
+    # Código que puede generar una excepción
+    resultado = 10 / 0  # División por cero
+    print(resultado)
+except ZeroDivisionError:
+    print("Error: División por cero")
+    
+"""
+Except
+El bloque except especifica el tipo de excepción que se desea capturar y manejar. Puedes tener múltiples bloques except para manejar diferentes tipos de excepciones.
+"""
+try:
+    # Código que puede generar una excepción
+    resultado = 10 / 0  # División por cero
+    print(resultado)
+except ZeroDivisionError:
+    print("Error: División por cero")
+except ValueError:
+    print("Error: Valor inválido")
+    
+"""
+Finally
+El bloque finally es opcional y se ejecuta siempre, independientemente de si ocurrió una excepción o no. Se utiliza comúnmente para realizar tareas de limpieza o liberación de recursos
+"""
+
+try:
+    # Código que puede generar una excepción
+    archivo = open("archivo.txt", "r")
+    # Realizar operaciones con el archivo
+except FileNotFoundError:
+    print("Error: Archivo no encontrado")
+finally:
+    archivo.close()  # Cerrar el archivo siempre, incluso si ocurre una excepción
+    
