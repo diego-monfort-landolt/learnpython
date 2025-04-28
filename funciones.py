1a3770dcfe8dff53e6566ba5cc732be5c40f3d30
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-04-28 09:19:51 +0200

    funktion die beshrieben ist für wo mit komentar in der funktion

--- funciones.py
+++ funciones.py
@@ -45,3 +45,28 @@ print("media: ", calcular_media(10, 20, 30, 40, 50))  # Imprime 3.0
 def sumar_3(x):
     return x + 3
 print('Sumarle 3 a und numero: ', sumar_3(5))  # Imprime 8
+
+def area_rectangulo(base, altura):
+    """
+    Calcula el área de un rectángulo.
+
+
+    Args:
+        base (float): La base del rectángulo.
+        altura (float): La altura del rectángulo.
+
+
+    Returns:
+        float: El área del rectángulo.
+    """
+    return base * altura
+  
+def suma_variable(*numeros):
+    total = 0
+    for numero in numeros:
+        total += numero
+    return total
+
+
+print(suma_variable(1, 2, 3))  # Imprime 6
+print(suma_variable(4, 5, 6, 7))  # Imprime 22
