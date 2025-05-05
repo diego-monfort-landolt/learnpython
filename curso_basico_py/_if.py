d5ce97f584a031fe559a787dd7ce0142085d6d34
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-05-05 21:23:55 +0200

    nochmals ein par versuche gemacht

--- curso_basico_py/_if.py
+++ curso_basico_py/_if.py
@@ -1,3 +1,7 @@
+import os
+# limpiar la consola despues de ejecutar el programa
+os.system('clear')
+
 
 nota = 5
 if nota >= 9:
@@ -8,3 +12,22 @@ elif nota >= 5:
   print('Regular')
 else:
   print('Vuelvo a estudiar')
+  
+edad = 19
+tienes_carnet = True
+if edad >= 18 and tienes_carnet:
+  print('Puedes conducir')
+else: 
+  print('No puedes conducir')
+  # con or solo tiene que estar una condicion en true para aceptar
+  if edad >= 18 or tienes_carnet:
+    print('Puedes conducir aqui')
+  else:
+    print('Paga la multa, YA!')
+    
+    # con not se niega la condicion
+es_fin_de_semana = False
+if not es_fin_de_semana:
+  print('A trabajar!')
+else:
+  print('Porfin llego el fin de semana')
