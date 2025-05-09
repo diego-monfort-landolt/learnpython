d0f967875fce9cfb27f001833ba1fee8a4cd314d
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-05-09 22:17:10 +0200

    hinzufügen separat gespeicherten datei

--- curso_basico_py/datos-compuestos.py
+++ curso_basico_py/datos-compuestos.py
@@ -7,3 +7,14 @@ print(lista[1]) # Imprime el segundo elemento de la lista
 
 print(type(1234)) # Imprime el tipo de dato de la cadena
 
+lista = ['Soy Diego', 'Quiero trabajar ' ' en Python', ' Soy programador', True, 1.75, ' cm']
+#creando un diccionario
+diccionario = {
+    'nombre': 'Diego',
+    'apellido': 'DEV',
+    'edad': 25,
+    'altura': 1.75,
+    'es_programador': True
+}
+print(diccionario['nombre']) # Imprime el valor asociado a la clave 'nombre'
+print(lista[4]) # Imprime el valor asociado a la clave 4
