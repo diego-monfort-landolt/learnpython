60cfb5b531036bb132c72400c91cdfd1c09a6d3b
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-08-07 20:59:08 +0200

    fast api erstellt und mit docs interface getestet

--- backend/api/fastapi/main.py
+++ backend/api/fastapi/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 # http://127.0.0.1:8000/
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+    editorial: Optional[str] 
 
 @app.get("/")
 def index():
@@ -11,3 +19,8 @@ def index():
 @app.get("/users/{id}")
 def mostrar_usuario(id: int):
   return {"data": id}
+
+
+@app.post("/users/")
+def mostrar_usuario(user: User):
+  return {"message": f"User: {user.name} is registred"}
